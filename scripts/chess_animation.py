f6fc0b416219c8c475b988e620305e4ab6fadeb4
John Bampton <jbampton@users.noreply.github.com>
2025-08-05 20:29:17 +1000

    Add Python isort with pre-commit (#340)

--- scripts/chess_animation.py
+++ scripts/chess_animation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import time
 import os
+import time
 
 FRAME_DELAY = 1  # delay in seconds between frames
 
--- scripts/chess_animation_2.py
+++ scripts/chess_animation_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import time
 import os
+import time
 
 # --- Configuration ---
 ANIMATION_DURATION_SECONDS = 10
